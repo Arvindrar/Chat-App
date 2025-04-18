73fd9f2c61a9094820ae84744ed5178b9507e992
Arvindrar <arvindangular@gmail.com>
2025-04-18 16:13:36 +0530

    🚀 Trigger Firebase deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Buffer } from "buffer";
+import process from "process";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+if (!window.Buffer) window.Buffer = Buffer;
+if (!window.process) window.process = process;
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
